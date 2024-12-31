c295d160b86f1cd8d9b7de1d66c6a781738eacc4
Atchayamanimaran <matchaya217@gmail.com>
2024-12-31 11:04:13 +0530

    Create String reverse.c

---
+++ String reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int length=0;
+    scanf("%s",str);
+    for(int i=0;str[i]!='\0';i++){
+        length++;
+    }
+    printf("The reversed string is ");
+    for (int i=length-1;i>=0;i--){
+        printf("%c",str[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
